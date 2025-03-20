6cb7a1b15203fa54522f822d4eea33b16014dfe2
jacobpanov <jacobpanov@gmail.com>
2025-03-20 18:56:58 -0400

    Adding 5 Random Packet Generation

--- uvma_training_1.2.6rev1/uvm/lab03_uvc/tb/router_test_lib.sv
+++ uvma_training_1.2.6rev1/uvm/lab03_uvc/tb/router_test_lib.sv
@@ -14,13 +14,19 @@ class base_test extends uvm_test;
 
     function void build_phase(uvm_phase phase);
         super.build_phase(phase);
+        uvm_config_wrapper::set(this, "tb.yapp.tx_agent.sequencer.run_phase",
+                                 "default_sequence",
+                                 yapp_5_packets::get_type());
         tb = new("tb", this);
-        `uvm_info("MSG", "\nTest build phase executed\n", UVM_HIGH)
     endfunction : build_phase
 
     function void end_of_elaboration_phase(uvm_phase phase);
-    uvm_top.print_topology();
-  endfunction : end_of_elaboration_phase
+        uvm_top.print_topology();
+    endfunction : end_of_elaboration_phase
+
+    function void start_of_simulation_phase(uvm_phase phase);
+      `uvm_info(get_type_name(), {"start of simulation for ", get_full_name()}, UVM_HIGH)
+    endfunction : start_of_simulation_phase
 
 endclass : base_test
 
