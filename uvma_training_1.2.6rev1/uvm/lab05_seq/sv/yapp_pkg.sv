69d739c0028a99253eb3fda926f40bba5a5e32b3
jacobpanov <jacobpanov@gmail.com>
2025-03-20 21:46:29 -0400

    Lab05 Work

---
+++ uvma_training_1.2.6rev1/uvm/lab05_seq/sv/yapp_pkg.sv
@@ -0,0 +1,17 @@
+// Jacob Panov
+// lab01_data yapp UVC package for accelerated UVM
+// yapp_pkg.sv
+
+package yapp_pkg;
+
+import uvm_pkg::*;
+`include "uvm_macros.svh"
+`include "yapp_packet.sv"
+`include "yapp_tx_monitor.sv"
+`include "yapp_tx_sequencer.sv"
+`include "yapp_tx_seqs.sv"
+`include "yapp_tx_driver.sv"
+`include "yapp_tx_agent.sv"
+`include "yapp_env.sv"
+  
+endpackage
