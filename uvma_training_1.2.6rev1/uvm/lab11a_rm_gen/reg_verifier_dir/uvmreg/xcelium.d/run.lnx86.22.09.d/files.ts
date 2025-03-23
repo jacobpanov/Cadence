500dc3083795d9f4638dc2befdf798dd1d80f4ab
jacobpanov <jacobpanov@gmail.com>
2025-03-22 21:07:08 -0400

    Using reg_verifier tool

---
+++ uvma_training_1.2.6rev1/uvm/lab11a_rm_gen/reg_verifier_dir/uvmreg/xcelium.d/run.lnx86.22.09.d/files.ts
@@ -0,0 +1,3 @@
+1742691576 /home/jpanov/Cadence/uvma_training_1.2.6rev1/uvm/lab11a_rm_gen/reg_verifier_dir/uvmreg/cdns_uvmreg_utils_pkg.sv
+1742691576 /home/jpanov/Cadence/uvma_training_1.2.6rev1/uvm/lab11a_rm_gen/reg_verifier_dir/uvmreg/yapp_router_regs_rdb.sv
+1742691766 /home/jpanov/Cadence/uvma_training_1.2.6rev1/uvm/lab11a_rm_gen/reg_verifier_dir/uvmreg/quicktest.sv
