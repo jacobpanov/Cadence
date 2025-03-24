7deeac67ba40d38722a2f005f37603cc578daceb
jacobpanov <84049465+jacobpanov@users.noreply.github.com>
2025-03-23 21:02:06 -0400

    Add cds_rnm_pkg.sv

---
+++ SVRNM_dx/common/packages/cds_rnm_pkg.sv
@@ -0,0 +1,21 @@
+package cds_rnm_pkg;
+
+   // Equivalent to VAMS wreal1drv
+   nettype real wreal1driver with CDS_res_wreal1driver;
+
+   // Equivalent to VAMS wreal4state
+   nettype real wreal4state with CDS_res_wreal4state;
+
+   // Equivalent to VAMS wrealmin
+   nettype real wrealmin with CDS_res_wrealmin;
+
+   // Equivalent to VAMS wrealmax
+   nettype real wrealmax with CDS_res_wrealmax;
+
+   // Equivalent to VAMS wrealsum
+   nettype real wrealsum with CDS_res_wrealsum;
+
+   // Equivalent to VAMS wrealavg
+   nettype real wrealavg with CDS_res_wrealavg;
+
+endpackage : cds_rnm_pkg
