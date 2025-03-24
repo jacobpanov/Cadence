d3836c0790dd098441e1ab4e9cf4a78ff3ef7c40
jacobpanov <jacobpanov@gmail.com>
2025-03-24 19:35:10 -0400

    Finishing Session__07

--- SVRNM_dx/exercises/Session_07/tb/xcelium.d/run.lnx8664.22.09.d/files.ts
+++ SVRNM_dx/exercises/Session_07/tb/xcelium.d/run.lnx8664.22.09.d/files.ts
@@ -3,4 +3,4 @@
 1742845399 /home/jpanov/Cadence/SVRNM_dx/exercises/Session_07/src/myRes.sv
 1742858486 /home/jpanov/Cadence/SVRNM_dx/exercises/Session_07/src/myVIR.sv
 1742845399 /home/jpanov/Cadence/SVRNM_dx/exercises/Session_07/src/myVRdrvG.sv
-1742845399 /home/jpanov/Cadence/SVRNM_dx/exercises/Session_07/tb/myEEcheck.sv
+1742858637 /home/jpanov/Cadence/SVRNM_dx/exercises/Session_07/tb/myEEcheck.sv
