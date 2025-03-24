55fd5ad398867af25375c76c4e1004fd069101ec
jacobpanov <jacobpanov@gmail.com>
2025-03-24 17:35:31 -0400

    Completing Session_05

--- SVRNM_dx/exercises/Session_05/tb/xcelium.d/run.lnx8664.22.09.d/files.ts
+++ SVRNM_dx/exercises/Session_05/tb/xcelium.d/run.lnx8664.22.09.d/files.ts
@@ -1,4 +1,4 @@
 1742778135 /home/jpanov/Cadence/SVRNM_dx/common/packages/cds_rnm_pkg.sv
 1742845399 /home/jpanov/Cadence/SVRNM_dx/exercises/Session_05/src/vco_dig.sv
-1742845399 /home/jpanov/Cadence/SVRNM_dx/exercises/Session_05/src/vco_sine.sv
-1742850706 /home/jpanov/Cadence/SVRNM_dx/exercises/Session_05/tb/vco_ds_TB.sv
+1742851557 /home/jpanov/Cadence/SVRNM_dx/exercises/Session_05/src/vco_sine.sv
+1742851557 /home/jpanov/Cadence/SVRNM_dx/exercises/Session_05/tb/vco_ds_TB.sv
