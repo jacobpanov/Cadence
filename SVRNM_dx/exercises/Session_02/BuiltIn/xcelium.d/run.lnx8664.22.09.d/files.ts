54371ed416a18b27bcdba50566758a78c992aa8f
jacobpanov <jacobpanov@gmail.com>
2025-03-23 21:32:09 -0400

    Session_02 Complete

---
+++ SVRNM_dx/exercises/Session_02/BuiltIn/xcelium.d/run.lnx8664.22.09.d/files.ts
@@ -0,0 +1,2 @@
+1742778235 /home/jpanov/Cadence/SVRNM_dx/exercises/Session_02/BuiltIn/cds_rnm_pkg.sv
+1730987510 /home/jpanov/Cadence/SVRNM_dx/exercises/Session_02/BuiltIn/temp.sv
---
+++ SVRNM_dx/exercises/Session_02/SVUDN_BuiltinResFunc/xcelium.d/run.lnx8664.22.09.d/.timestamp.ts
@@ -0,0 +1,2 @@
+1742778135 /home/jpanov/Cadence/SVRNM_dx/common/packages/cds_rnm_pkg.sv
+1592846868 /home/jpanov/Cadence/SVRNM_dx/exercises/Session_02/SVUDN_BuiltinResFunc/temp.sv
