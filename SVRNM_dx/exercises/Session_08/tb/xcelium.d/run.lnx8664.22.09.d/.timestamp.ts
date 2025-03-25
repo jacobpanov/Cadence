aa205395d0e756ee172ea7178b05e82a22c40872
jacobpanov <jacobpanov@gmail.com>
2025-03-24 20:43:09 -0400

    Updating EEnet

--- SVRNM_dx/exercises/Session_08/tb/xcelium.d/run.lnx8664.22.09.d/.timestamp.ts
+++ SVRNM_dx/exercises/Session_08/tb/xcelium.d/run.lnx8664.22.09.d/.timestamp.ts
@@ -1,3 +1,3 @@
-1742845398 /home/jpanov/Cadence/SVRNM_dx/exercises/Session_08/tb/VRdiff_tb.sv
+1742863081 /home/jpanov/Cadence/SVRNM_dx/exercises/Session_08/src/VRdiffGDU.sv
+1742863081 /home/jpanov/Cadence/SVRNM_dx/exercises/Session_08/tb/VRdiff_tb.sv
 1742857672 /home/jpanov/Cadence/SVRNM_dx/common/packages/EE_pkg.sv
-1742845398 /home/jpanov/Cadence/SVRNM_dx/exercises/Session_08/src/VRdiffGD0.sv
