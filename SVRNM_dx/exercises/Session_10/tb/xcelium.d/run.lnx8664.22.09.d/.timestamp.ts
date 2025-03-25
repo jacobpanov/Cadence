84fd0ef7dc8c20842058ec03dce65a1502bf9bf7
jacobpanov <jacobpanov@gmail.com>
2025-03-25 17:41:40 -0400

    Finishing Session_10

---
+++ SVRNM_dx/exercises/Session_10/tb/xcelium.d/run.lnx8664.22.09.d/.timestamp.ts
@@ -0,0 +1,5 @@
+1742845401 /home/jpanov/Cadence/SVRNM_dx/exercises/Session_10/src/NMOSdioV.sv
+1742845401 /home/jpanov/Cadence/SVRNM_dx/exercises/Session_10/src/NMOSdioG.sv
+1742845401 /home/jpanov/Cadence/SVRNM_dx/exercises/Session_10/src/NMOSdioI.sv
+1742857672 /home/jpanov/Cadence/SVRNM_dx/common/packages/EE_pkg.sv
+1742845401 /home/jpanov/Cadence/SVRNM_dx/exercises/Session_10/tb/NMOS_tb.sv
---
+++ SVRNM_dx/exercises/Session_10/tb/xcelium.d/run.lnx8664.22.09.d/files.ts
@@ -0,0 +1,2 @@
+1742857672 /home/jpanov/Cadence/SVRNM_dx/common/packages/EE_pkg.sv
+1742845401 /home/jpanov/Cadence/SVRNM_dx/exercises/Session_10/tb/NMOS_tb.sv
